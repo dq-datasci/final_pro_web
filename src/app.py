b86ba47a162856fbfad32d8e60e09a16fcd6996d
dq-datasci <david.a.quintela@gmail.com>
2025-06-15 00:57:44 -0400

    feat: cree perfil.html y añadi su ruta
    -Añadí el boton de perfil debajo de galeria, en index, perfil y resumen,
    para que cuando hagan click redirija a perfil.html
    - Agregue la ruta de perfil en app.py
    - cree perfil.html con la base del código de index.html, tengo que
     redireccionar los datos que se estan llamando de app.py

--- src/app.py
+++ src/app.py
@@ -39,6 +39,10 @@ def resumen():
 def galeria():
     return render_template("galeria.html")
 
+@app.route('/perfil')
+def perfil():
+    return render_template("perfil.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
